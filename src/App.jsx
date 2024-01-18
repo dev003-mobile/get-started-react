62472f5167e1815a9da580606cbc43608bb1d6fb
Wagner Coche <wagnercoche@gmail.com>
2024-01-18 20:17:49 +0100

    🐳 chore: Sidebar component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./global.css"
+import styles from "./App.module.css"
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <div> 
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>Menu</main>
+      </div>
     </div>
   )
 }
